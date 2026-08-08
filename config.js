ce016949059a10eddbbecd68fdfbc165790bfe90
systemworking84-blip <system.working84@gmail.com>
2026-08-07 22:35:57 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// CONFIGURACIÓN DE LA INVITACIÓN
+// Cambia SOLO el número entre comillas, usando código de país y sin espacios.
+// México: 52 + número de 10 dígitos.
+const WHATSAPP_NUMBER = "526692774102";
+
+const WHATSAPP_MESSAGE =
+  "Hola, confirmo mi asistencia a la fiesta de José Luis y Santiago. ⚽🏊";
